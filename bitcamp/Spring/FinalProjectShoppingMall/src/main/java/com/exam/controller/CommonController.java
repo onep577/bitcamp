a5cce5a6f2ee88bfaee2396bd4fe1f638d4ca143
onep577 <onep577@naver.com>
2019-09-24 09:41:04 +0900

    spring

--- bitcamp/Spring/FinalProjectShoppingMall/src/main/java/com/exam/controller/CommonController.java
+++ bitcamp/Spring/FinalProjectShoppingMall/src/main/java/com/exam/controller/CommonController.java
@@ -1,6 +1,11 @@
 package com.exam.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +13,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import com.exam.model.ExamDTO;
 
 @Controller
